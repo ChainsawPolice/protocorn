b8110123a2f450930cce0821cd5592ba094cf16c
Joshua Merriman <chainsaw.police@gmail.com>
2015-05-19 09:18:42 +1000

    Cleaned up some things
    Made overall window size easily adjustable via changing the factor
    variable
    
    Merged pixel_coords and pixel_colours into one list - matrix.
    
    Implemented off() and clear()
    
    Tidied up README.md
    
    Shaved off unneeded imports. Is that even comment-worthy?

--- README.md
+++ README.md
@@ -6,10 +6,18 @@ Protocorn allows you to test Unicorn HAT projects on your Windows, Mac, or Linux
 Protocorn is intended as a drop-in replacement for the `unicornhat` module — just drop protocorn.py into your project folder, and wherever you have `import unicornhat` in your code, simply replace it with `import protocorn as unicornhat` and run your script.
 
 ## Dependencies
-You will need to have PyGame installed for your OS. It's farirly easy on Windows and Linux, but OS X requires some major fiddling and tweaking. I wish I could tell you how I got it working on my Mac, but I'd be lying if I said I knew how I did it.
+You will need to have PyGame installed for your OS. It's fairly easy on Windows and Linux, but OS X requires some major fiddling and tweaking.  
+I wish I could tell you how I got it working on my Mac, but I'd be lying if I said I knew how I did it.
 
 ## Coverage
-Currently Protocorn covers the `set_pixel()` and `show()` functions from the official `unicornhat` module. Other modules are not supported as of yet. However most, if not all, functions will be implemented.
+Currently Protocorn covers the following functions from the official `unicornhat` module:
+
+ * `set_pixel()`
+ * `show()`
+ * `clear()`
+ * `off()`
+
+Other modules are not supported as of yet. However most, if not all, functions will be implemented in the future.  
 
 ## To do
 * Create PyPi package
--- protocorn.py
+++ protocorn.py
@@ -5,49 +5,63 @@ if __name__ == '__main__':
 
 else:
 	import pygame
-	from random import randint
 	from pygame.locals import *
 
-	factor = 70
+	factor        = 20
+	size          = 8*factor
 
 	pygame.init()
 	pygame.display.set_caption('🐴')
-	DISPLAYSURF = pygame.display.set_mode(((8*factor-2), (8*factor-2)))
+	DISPLAYSURF = pygame.display.set_mode(((size-2), (size-2)))
 	DISPLAYSURF.fill((20,20,20))
 
-	# Build pixel matrix
-	pixel_coords = []
-	for x in range(0,(8*factor), factor):
+	######## Build pixel matrix ########
+	# The "LEDs" are stored in a 2D list, with each index containing a pygame.Rect() object and a colour tuple.
+	# Referencing matrix[1][3] will pull up the Rect() object (at [0]) and the colours (at [1]) of the "LED" at x1 y3 in a two-item list.
+	matrix = []
+	for x in range(0,size, factor):
 		temp_row = []
-		for y in range(0,(8*factor), factor):
-			temp_row.append(pygame.Rect(x,y,factor-2,factor-2))
-		pixel_coords.append(temp_row)
+		for y in range(0,(size), factor):
+			temp_row.append([pygame.Rect(x,y,factor-2,factor-2), (0,0,0)]) # All pixels start off as pure black.
+		matrix.append(temp_row)
 
-	# Build color matrix
-	pixel_colours = []
-	for x in range(0,8):
-		temp_row = []
-		for y in range(0,8):
-			temp_row.append((0,0,0))
-		pixel_colours.append(temp_row)
+	######## unicornhat functions ########
 
 	def set_pixel(x,y, r,g,b):
-		pixel_colours[x][y] = (r,g,b)
+		''' Set a single pixel to RGB colour '''
+		matrix[x][y][1] = (r,g,b)
 
 	def show():
-		for y, row in enumerate(pixel_coords):
-			for x, neopixel in enumerate(row):
-				pygame.draw.rect(DISPLAYSURF, (pixel_colours[x][y]), neopixel)
+		''' Update UnicornHat with the contents of the display buffer '''
+		for row in matrix:
+			for neopixel in row:
+				pygame.draw.rect(DISPLAYSURF, neopixel[1], neopixel[0])
 		pygame.display.update()
 
-	def brightness(brightness): print "brightness: Not implemented yet, sorry!"
-	def rotation(rotation): print "rotation: Not implemented yet, sorry!"
-	def get_brightness(x): print "get_brightness: Not implemented yet, sorry!"
-	def clear(x): print "clear: Not implemented yet, sorry!"
-	def off(x): print "off: Not implemented yet, sorry!"
-	def get_index_from_xy(x): print "get_index_from_xy: Not implemented yet, sorry!"
-	def get_pixel(x): print "get_pixel: Not implemented yet, sorry!"
-	def set_pixels(x): print "set_pixels: Not implemented yet, sorry!"
-	def get_pixels(x): print "get_pixels: Not implemented yet, sorry!"
+	def clear():
+		''' Clear the buffer '''
+		for row in matrix:
+			for neopixel in row:
+				pygame.draw.rect(DISPLAYSURF, (0,0,0), neopixel[0])
+
+	def off():
+		''' Clear the buffer and immediately update UnicornHat to turn off all pixels. '''
+		clear()
+		show()
+
+	def _matrix():
+		''' Print out the matrix of "pixels". For my own personal testing. Not to be shipped. '''
+		for row in matrix:
+			print row
+
+	######## Non-implemented functions ########
+
+	def brightness(brightness): print "brightness(): Not implemented yet, sorry!"
+	def rotation(rotation): print "rotation(): Not implemented yet, sorry!"
+	def get_brightness(x): print "get_brightness(): Not implemented yet, sorry!"
+	def get_index_from_xy(x): print "get_index_from_xy(): Not implemented yet, sorry!"
+	def get_pixel(x): print "get_pixel(): Not implemented yet, sorry!"
+	def set_pixels(x): print "set_pixels(): Not implemented yet, sorry!"
+	def get_pixels(x): print "get_pixels(): Not implemented yet, sorry!"
 
 	show()
